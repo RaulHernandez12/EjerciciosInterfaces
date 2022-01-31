4933b827c2cb98ab259e4b107d4e72c520dca10f
Raul <rhc0001@alu.medac.es>
2022-01-31 16:35:03 +0100

    validacion con jquery

--- js/validar.js
+++ js/validar.js
@@ -1,85 +1,162 @@
-function validar() {
-    var dni, nombre, apellido, correo, pas , repetir , cp  ;
-    var expresionDni = /^\d{8}[a-zA-Z]$/;
-    var expresionNombreApellido = /[a-z]/;
-    var expresionCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    var expresionPas = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
-    var expresionCp = /^(?:0[1-9]\d{3}|[1-4]\d{4}|5[0-2]\d{3})$/;
-    
-    dni = document.forms["formulario"]["dni"].value;
-    nombre = document.forms["formulario"]["nombre"].value;
-    apellido = document.forms["formulario"]["apellido"].value;
-    correo = document.forms["formulario"]["correo"].value;
-    pas = document.forms["formulario"]["pas"].value;
-    repetir = document.forms["formulario"]["repetir"].value;
-    cp = document.forms["formulario"]["cp"].value;
-    var comentarios = document.getElementById("area").value;
-    var palabras = comentarios.trim().split(" ");
-    
-    
-    
+
+$(document).ready( function(){
    
-        if( dni === "" || nombre === "" || apellido === "" || correo === "" || pas === "" || repetir === "" || cp === "" || comentarios === ""){
-            document.getElementById("todos").innerHTML = "Todos los campos son obligatorios"
+    $("#boton").click(function(){    
+        
+        var expresionDni = /^\d{8}[a-zA-Z]$/;
+        var expresionNombreApellido = /[a-z]/;
+        var expresionCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        var expresionPas = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+        var expresionCp = /^(?:0[1-9]\d{3}|[1-4]\d{4}|5[0-2]\d{3})$/;
+
+        var dni = $("#dni").val();
+        var nombre = $("#nombre").val();   
+        var apellido = $("#apellido").val();
+        var correo = $("#correo").val();
+        var pas = $("#pas").val();
+        var repetir = $("#repetir").val();
+        var cp = $("#cp").val();
+        var comentarios = $("#area").val();
+        var palabras = comentarios.trim().split(" ");
+
+        if(dni === "" || nombre === "" || apellido === "" || correo === "" || pas === "" || repetir === "" || cp === "" || comentarios === ""){
+            $("#todos").text( "Todos los campos son obligatorios");
             return false;
-        };
+        }
 
         if(!expresionDni.test(dni)){
-            document.getElementById("dniP").innerHTML ="DNI incorrecto";
-            return false;           
-        }
+            $("#dniP").text("DNI incorrecto");          
+            }
         else{
-            document.getElementById("dniP").innerHTML ="DNI correcto";
-        }; 
-
+                $("#dniP").text("DNI correcto");
+            };
+        
         if(!expresionNombreApellido.test(nombre)){
-            document.getElementById("nombreP").innerHTML ="El nombre no puede contener numeros";
-            return false;
+            $("#nombreP").text("El nombre no puede contener numeros");
         }else{
-            document.getElementById("nombreP").innerHTML ="El nombre es correcto";
-        };
-
+                $("#nombreP").text("El nombre es correcto");
+            };
         if(!expresionNombreApellido.test(apellido)){
-            document.getElementById("apellidoP").innerHTML ="El apellido no puede contener numeros";
-            return false;
+            $("#apellidoP").text("El apellido no puede contener numeros");
         }else{
-            document.getElementById("apellidoP").innerHTML ="El apellido es correcto";
-        };
-
+                $("#apellidoP").text("El apellido es correcto");
+            };
         if(!expresionCorreo.test(correo)){
-            document.getElementById("correoP").innerHTML ="El correo no es valido";
+            $("#correoP").text("El correo no es valido") ;
             return false;
         }else{
-            document.getElementById("correoP").innerHTML ="El correo es valido";
-        };
+            $("#correoP").text("El correo es valido");
+            };
 
         if(!expresionPas.test(pas)){
-            document.getElementById("pasP").innerHTML ="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.";
+            $("#pasP").text("La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.") ;
             return false;
         }else{
-            document.getElementById("pasP").innerHTML ="La contraseña es valida";
-        };
+            $("#pasP").text("La contraseña es valida");
+            };
 
         if(pas !== repetir){
-            document.getElementById("repetirP").innerHTML ="Las contraseñas deben coincidir";
+            $("#repetirP").text("Las contraseñas deben coincidir");
             return false;
         }else{
-            document.getElementById("repetirP").innerHTML ="Las contraseñas coinciden correctamente";
-        };
+            $("#repetirP").text("Las contraseñas coinciden correctamente");
+            };
         
         if(!expresionCp.test(cp)){
-            document.getElementById("cpP").innerHTML ="El codigo postal no es valido";
+            $("#cpP").text("El codigo postal no es valido");
             return false;
         }else{
-            document.getElementById("cpP").innerHTML ="El codigo postal es valido";
-        };
-        console.log(palabras.length)
+            $("#cpP").text("El codigo postal es valido");
+            };
         for(var i = 0; i < palabras.length ; i++ ){
-            if(palabras[i] == "tonto" || palabras[i] == "idiota" || palabras[i] == "gilipollas"){
-                alert("No se permiten insultos");
-                return false;
+            if(palabras[i].toLowerCase() == "tonto" || palabras[i].toLowerCase() == "idiota" || palabras[i].toLowerCase() == "gilipollas"){
+                $("#areaP").text("No se permiten insultos");    
+                return false;           
             }
-            
         }
+    })
+} );
+
+// function validar() {
+//     var dni, nombre, apellido, correo, pas , repetir , cp  ;
+//     var expresionDni = /^\d{8}[a-zA-Z]$/;
+//     var expresionNombreApellido = /[a-z]/;
+//     var expresionCorreo = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+//     var expresionPas = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+//     var expresionCp = /^(?:0[1-9]\d{3}|[1-4]\d{4}|5[0-2]\d{3})$/;
+    
+//     dni = document.forms["formulario"]["dni"].value;
+//     nombre = document.forms["formulario"]["nombre"].value;
+//     apellido = document.forms["formulario"]["apellido"].value;
+//     correo = document.forms["formulario"]["correo"].value;
+//     pas = document.forms["formulario"]["pas"].value;
+//     repetir = document.forms["formulario"]["repetir"].value;
+//     cp = document.forms["formulario"]["cp"].value;
+//     var comentarios = document.getElementById("area").value;
+//     var palabras = comentarios.trim().split(" ");
+    
+   
+//         if( dni === "" || nombre === "" || apellido === "" || correo === "" || pas === "" || repetir === "" || cp === "" || comentarios === ""){
+//             document.getElementById("todos").innerHTML = "Todos los campos son obligatorios"
+//             return false;
+//         };
+
+//         if(!expresionDni.test(dni)){
+//             document.getElementById("dniP").innerHTML ="DNI incorrecto";
+//             return false;           
+//         }
+//         else{
+//             document.getElementById("dniP").innerHTML ="DNI correcto";
+//         }; 
+
+//         if(!expresionNombreApellido.test(nombre)){
+//             document.getElementById("nombreP").innerHTML ="El nombre no puede contener numeros";
+//             return false;
+//         }else{
+//             document.getElementById("nombreP").innerHTML ="El nombre es correcto";
+//         };
+
+//         if(!expresionNombreApellido.test(apellido)){
+//             document.getElementById("apellidoP").innerHTML ="El apellido no puede contener numeros";
+//             return false;
+//         }else{
+//             document.getElementById("apellidoP").innerHTML ="El apellido es correcto";
+//         };
+
+//         if(!expresionCorreo.test(correo)){
+//             document.getElementById("correoP").innerHTML ="El correo no es valido";
+//             return false;
+//         }else{
+//             document.getElementById("correoP").innerHTML ="El correo es valido";
+//         };
+
+//         if(!expresionPas.test(pas)){
+//             document.getElementById("pasP").innerHTML ="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.";
+//             return false;
+//         }else{
+//             document.getElementById("pasP").innerHTML ="La contraseña es valida";
+//         };
+
+//         if(pas !== repetir){
+//             document.getElementById("repetirP").innerHTML ="Las contraseñas deben coincidir";
+//             return false;
+//         }else{
+//             document.getElementById("repetirP").innerHTML ="Las contraseñas coinciden correctamente";
+//         };
         
-}
+//         if(!expresionCp.test(cp)){
+//             document.getElementById("cpP").innerHTML ="El codigo postal no es valido";
+//             return false;
+//         }else{
+//             document.getElementById("cpP").innerHTML ="El codigo postal es valido";
+//         };
+//         console.log(palabras.length)
+//         for(var i = 0; i < palabras.length ; i++ ){
+//             if(palabras[i] == "tonto" || palabras[i] == "idiota" || palabras[i] == "gilipollas"){
+//                 alert("No se permiten insultos");
+//                 return false;
+//             }
+            
+//         }
+        
+// }
